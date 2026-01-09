bc364ace7e7e3e45c20980f42fcd60a61e0d0f5c
oleksandrasafranchuk <safrancukoleksandra@gmail.com>
2026-01-09 15:08:05 +0200

    Resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Header from "../components/Header/Header";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
